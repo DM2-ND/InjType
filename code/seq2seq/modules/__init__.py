3b4e9bf3ffbcf754dfdbab4245bf03ff6eb92fa9
Wenhao <wyu@Wenhaos-iMac.local>
2021-02-07 14:44:23 -0500

    update

--- code/seq2seq/modules/__init__.py
+++
@@ -1,5 +0,0 @@
-from seq2seq.modules.GlobalAttention import GlobalAttention
-from seq2seq.modules.ConcatAttention import ConcatAttention
-from seq2seq.modules.Maxout import MaxOut
-from seq2seq.modules.myAdam import MyAdam
-from seq2seq.modules.myRNN import MyGRU
